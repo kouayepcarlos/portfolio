a1355c107fe00936662ebd324ed3ebe9f2d76d58
Jocelyn Kouayep <65819970+kouayepcarlos@users.noreply.github.com>
2025-05-24 15:06:28 +0200

    Update portfolio.js

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -9,6 +9,12 @@ const images = [
     { src: './images/img1.png', alt: 'Ingenius-rm : solution pour vos questions d\'immigration', description: 'Nous sommes une Agence qui vous accompagne dans vos projet d\'immigration vers les destinations souhaitez, en vous proposant un suivi personnalisé dans le respect des Lois d\'immigration en vigueur et mettant en priorité votre satisfaction totale', lien: 'https://www.ingenius-rm.net/' },
     { src: './images/img2.png', alt: 'KrakOs : est le groupe de recherche en virtualisation, Conception d\'OS', description: 'Krakos est le groupe de recherche Virtualisation, Conception d\'OS, Programmation concurrente et distribuée, Programmation distribuée et Middleware pour les infrastructures cloud de l\'Université de Grenoble et l\'un des plus grands groupes du département Informatique de l\'Université de Grenoble', lien: 'https://lig-krakos.imag.fr/index.html' },
     { src: './images/img3.png', alt: 'Nilservice : Plateforme de prestation de service', description: 'NilService est une plateforme de solutions tout en un qui vous connecte à des professionnels de divers domaines allant des plus minimes services du quotidien aux travaux les plus complexes où que vous soyez en Afrique.', lien : 'nilservice.net' },
+
+    { src: './images/synergy.png', alt: 'Agri Synergy : Une meilleure agriculture pour un avenir meilleur', description: 'Nous cultivons principalement des cultures tropicales telles que le plantain, l’avocatier, les agrumes et le palmier à huile. Chaque culture est adaptée à notre climat et gérée selon des pratiques agricoles durables.', lien: 'https://synergy.nilservice.net/' },
+    { src: './images/dechet.png', alt: 'Gestion déchet 237 : Application mobile dedié à la gestion intelligente des déches', description: 'Cette application mobile a été conçue dans le cadre d’un projet technologique visant à améliorer la gestion des déchets urbains. Elle permet aux utilisateurs de localiser les points de collecte, de suivre le niveau de remplissage des bacs connectés', lien: '#' },
+    { src: './images/coquine.png', alt: 'Coquine Service : Rencontrez des escortes locales en ligne dès maintenant!', description: 'Nous pensons que chaque rencontre doit être une expérience mémorable. Nous sélectionnons soigneusement les profils sur notre site, en veillant à ce que chaque annonce reflète la qualité, le charme et le professionnalisme.', lien : 'https://coquineservice.com/' },
+
+    
     { src: './images/img5.png', alt: 'LabPress : la presse pour les laboratoire ', description: 'LabPress (presse de laboratoire) est le nom de la société que nous avons créée, proposant des services de communication scientifique (conseil, organisation d\'événements scientifiques, réalisation de magazines, webmastering, gestion des médias sociaux, création de sites Internet, etc.).', lien: 'http://labpress.scienceontheweb.net/' },
     { src: './images/img6.png', alt: 'Action Burundi ', description: 'Dans le souci d’exposer les étudiants et professionnels burundais aux technologies de pointe tant au niveau théorique que pratique, nous avons initié l\'école d’été “KWIGA” qui est une école d\'été organisée au Burundi, dont la première édition s’est tenue du 14 au 20 septembre 2023 à Bujumbura. Cette école est à l’initiative du collectif Ubuntu et du Professeur Daniel Hagimont, Professeur à l’Institut National Polytechnique de Toulouse.', lien: 'https://mlns2.github.io/ActionBurundi.io/' },
     { src: './images/img7.png', alt: 'Portfolio @Prof Tchana Alain', description: 'Portfolio @Prof Tchana Alain', lien : 'https://lig-membres.imag.fr/tchanaa/index.html' },
@@ -75,7 +81,7 @@ function Portfolio() {
 
         <div className="image-gallery" id='portfolio'>
          <h2>Portfolio</h2>
-         <p>Ci dessous, quelques exemples de réalisations effectuées durant la formation, le stage et plus encore :</p>
+         <p>Ci-dessous, quelques exemples de réalisations effectuées à titre personnel pour des clients :</p>
         <div className="row">
           {images.slice(startIndex, endIndex).map((image, index) => (
             <div key={index} className="col-md-4 image-item">
